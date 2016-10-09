12bf3360428bc4681fd31913e487cb2532bec8c3
Oliver <of9142@my.bristol.ac.uk>
2016-10-09 15:50:59 +0100

    change player function added

--- js/app.js
+++ js/app.js
@@ -1,45 +1,3 @@
-var flagsEasy = [{
-  image: "images/brazil.png",
-  country: "brazil",
-  capital: "brasilia"
-},{
-  image: "images/china.png",
-  country: "china",
-  capital: "beijing"
-},{
-  image: "images/france.png",
-  country: "france",
-  capital: "paris"
-},{
-  image: "images/germany.png",
-  country: "germany",
-  capital: "berlin"
-},{
-  image: "images/india.png",
-  country: "india",
-  capital: "new dehli"
-},{
-  image: "images/italy.png",
-  country: "italy",
-  capital: "rome"
-},{
-  image: "images/japan.png",
-  country: "japan",
-  capital: "tokyo"
-},{
-  image: "images/russia.png",
-  country: "russia",
-  capital: "moscow"
-},{
-  image: "images/uk.png",
-  country: "uk",
-  capital: "london"
-},{
-  image: "images/usa.png",
-  country: "usa",
-  capital: "washington dc"
-}];
-
 var flags = document.querySelectorAll('.board .flag img');
 var countryInputs = document.getElementsByClassName("countryInput");
 
@@ -73,11 +31,13 @@ shuffle();
 
 function checkAnswer (index) {
   var userAnswer = countryInputs[index].value;
-  console.log(userAnswer, flagsEasy[index].country);
   if (userAnswer === flagsEasy[index].country) {
     countryInputs[index].style.backgroundColor = "lightgreen";
+    switchTurn();
+    updateScores();
   } else {
     countryInputs[index].style.backgroundColor = "tomato";
+    switchTurn();
   }
 }
 
@@ -93,47 +53,40 @@ for (var i = 0; i < inputs.length; i++) {
   }(i));
 };
 
+function startGame() {
+  document.turn = "player 1";
+  setMessage(document.turn + "'s turn");
+}
 
-// update scores
-// var player1Score = 0;
-// var player2Score = 0;
-//
-// var updateScores = function() {
-//   if (checkAnswer ==
-//   document.getElementById("player1Score").innerHTML=player1Score;
-//   document.getElementById("player2Score").innerHTML=player2Score;
-// }
-// updateScores();
+function setMessage(msg) {
+  document.getElementById("playerTurn").innerText = msg;
+}
 
+function switchTurn() {
+  if (document.turn === "player 1") {
+    document.turn = "player 2";
+    setMessage(document.turn + "'s turn");
+  } else {
+    document.turn = "player 1";
+    setMessage(document.turn + "'s turn");
+  }
+}
 
+var player1Score = 0;
+var player2Score = 0;
+function updateScores(index) {
+  var userAnswer = countryInputs[index].value;
+  if (document.turn === "player 1" && userAnswer === flagsEasy[index].country) {
+    document.getElementById("player1Score").innerHTML=player1Score;
+    player1Score++;
+  } else {
+    (document.turn === "player 2" && userAnswer === flagsEasy[index].country);
+    document.getElementById("player2Score").innerHTML=player2Score;
+    player2Score++;
+  }
+}
 
-
-
-
-
-
-
-// //changes player
-// function changePlayer() {
-//   if (checkWinner(document.turn)) {
-//     setMessage(document.turn + " wins");
-//     document.gameOver = document.turn;
-//   } else if (document.turn == "X") {
-//     document.turn = "0";
-//     setMessage(document.turn + "'s turn");
-//   } else {
-//     document.turn = "X";
-//     setMessage(document.turn + "'s turn");
-//   }
-// }
-//
-// //indicates player turn
-// function currentTurn() {
-//   document.turn = "X";
-//   document.gameOver = null;
-//   setMessage(document.turn + "'s turn");
-// }
-//
-// function setMessage(msg) {
-//   document.getElementById("message").innerText = msg;
-// }
+// change player function
+// - starts with player 1 ---done
+// - when enter pressed change player ---done
+// - when enter pressed add 1 to score if answer is correct
---
+++ js/flags.js
@@ -0,0 +1,41 @@
+var flagsEasy = [{
+  image: "images/brazil.png",
+  country: "brazil",
+  capital: "brasilia"
+},{
+  image: "images/china.png",
+  country: "china",
+  capital: "beijing"
+},{
+  image: "images/france.png",
+  country: "france",
+  capital: "paris"
+},{
+  image: "images/germany.png",
+  country: "germany",
+  capital: "berlin"
+},{
+  image: "images/india.png",
+  country: "india",
+  capital: "new dehli"
+},{
+  image: "images/italy.png",
+  country: "italy",
+  capital: "rome"
+},{
+  image: "images/japan.png",
+  country: "japan",
+  capital: "tokyo"
+},{
+  image: "images/russia.png",
+  country: "russia",
+  capital: "moscow"
+},{
+  image: "images/uk.png",
+  country: "uk",
+  capital: "london"
+},{
+  image: "images/usa.png",
+  country: "usa",
+  capital: "washington dc"
+}];
