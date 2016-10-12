fe0267e1aa540f2f486f3514417efb642f921cbe
Oliver <of9142@my.bristol.ac.uk>
2016-10-12 09:09:27 +0100

    set up for heroku

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
 <?php include_once 'home.html' ?>
+<?php include_once 'easyflags.html' ?>
+<?php include_once 'mediumflags.html' ?>
+<?php include_once 'hardflags.html' ?>
+<?php include_once 'extrahardflags.html' ?>
+<?php include_once 'all.html' ?>
